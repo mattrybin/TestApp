6a9bfff159e77b5346579cf75abf01c43227efdf
Matt Rybin <workmateuszrybin@gmail.com>
2021-01-12 04:48:51 +0700

    chore: update

--- scripts/download-android.sh
+++ scripts/download-android.sh
@@ -1,11 +1,17 @@
-# curl -X GET "https://api.appcenter.ms/v0.1/apps/Epic-Money/Epic-Money/branches/master/builds" -H  "accept: application/json" -H  "X-API-Token: 2ce7d022f5532f0cb82334bba2c7418a7ddbfcca" | jq "."
-url=$(curl -X GET "https://api.appcenter.ms/v0.1/apps/Epic-Money/Epic-Money/builds/1/downloads/build" -H  "accept: application/json" -H  "X-API-Token: 2ce7d022f5532f0cb82334bba2c7418a7ddbfcca" | jq --raw-output '.uri')
+echo "> Check latest successful build on master"
+build_id=$(curl -X GET "https://api.appcenter.ms/v0.1/apps/Epic-Money/Epic-Money/branches/master/builds" -H  "accept: application/json" -H  "X-API-Token: 2ce7d022f5532f0cb82334bba2c7418a7ddbfcca" | jq '[.[] | {id,result} | select(.result=="succeeded")][0] | .id')
+echo ""
+echo "> Featching Download URL"
+url=$(curl -X GET "https://api.appcenter.ms/v0.1/apps/Epic-Money/Epic-Money/builds/$build_id/downloads/build" -H  "accept: application/json" -H  "X-API-Token: 2ce7d022f5532f0cb82334bba2c7418a7ddbfcca" | jq --raw-output '.uri')
+echo ""
+echo "> Downloading the build"
 sleep 10
-# curl -o and.zip ${url}
 curl ${url} > android-download.zip
+echo ""
+echo "> Unzipping and placing the build in the right place"
 mkdir -p builds
 mkdir -p builds/android
-unzip android-download.zip -d ./builds
+unzip -qq android-download.zip -d ./builds
 rm -f -- ./builds/android/app-debug.apk
 mv -f ./builds/build/app-debug.apk ./builds/android/app-debug.apk
 rmdir ./builds/build
