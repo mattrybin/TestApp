3eb20d044b57f59ab26e9136b29f5b4cd188ef26
Matt Rybin <workmateuszrybin@gmail.com>
2021-01-17 14:44:30 +0700

    chore: fix build

--- scripts/wait-on-app-center-build.sh
+++ scripts/wait-on-app-center-build.sh
@@ -1,8 +1,7 @@
-# Update
 export MOBILE_CENTER_CURRENT_APP=MattRybin/TestApp-1
 function check {
 INPUT=$(appcenter build branches list --output json)
-STATUS=$(echo $INPUT | jq --raw-output "[.[] | {buildNumber,status,result} | select(.status==\"completed\") | select(.buildNumber==\"$1\") | select(.sourceBranch==\"master\")]")
+STATUS=$(echo $INPUT | jq --raw-output "[.[] | {buildNumber,status,result,sourceBranch} | select(.status==\"completed\") | select(.buildNumber==\"$1\") | select(.sourceBranch==\"master\")]")
 if [ "$STATUS" == "[]" ]; then
     echo "Waiting on build number: $1"
     echo $INPUT
@@ -19,3 +18,10 @@ else
 fi
 }
 until check $1 ; do sleep 2 ; done
+
+# INPUT='[{"sourceBranch":"master","buildNumber":"31","status":"inProgress","author":"Matt Rybin <workmateuszrybin@gmail.com>","message":"chore: update","sha":"636f17d1b6a4dc54f5b4f9da9b912ec5fe790f9e","url":"https://appcenter.ms/users/MattRybin/apps/TestApp-1/build/branches/master/builds/31"},{"sourceBranch":"next","buildNumber":"15","status":"completed","result":"succeeded","author":"github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>","message":"[CI] Merged refs/heads/master into target next","sha":"c10263ec6d55e323057620eff658bf525db19487","url":"https://appcenter.ms/users/MattRybin/apps/TestApp-1/build/branches/next/builds/15"}]'
+# # INPUT='[{"hello":"people"}]'
+# # STATUS=$(echo $INPUT | jq --raw-output "[.[] | {buildNumber,status,result} | select(.status==\"completed\") | select(.buildNumber==\"31\") | select(.sourceBranch==\"master\")]")
+# # STATUS=$(echo $INPUT | jq --raw-output "[.[] | {buildNumber,status,result} | select(.sourceBranch==\"master\") | select(.buildNumber==\"31\")]")
+# STATUS=$(echo $INPUT | jq --raw-output "[.[] | {buildNumber,status,result,sourceBranch} | select(.sourceBranch==\"master\")]")
+# echo $STATUS
