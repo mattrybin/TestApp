7b7fa98eae7fb720875672f01df4ab2d90ebc5aa
Matt Rybin <workmateuszrybin@gmail.com>
2021-01-18 08:59:54 +0700

    chore: update

--- app/services/firebase.tsx
+++ app/services/firebase.tsx
@@ -2,7 +2,7 @@ import '@react-native-firebase/app'
 import firestore from '@react-native-firebase/firestore'
 import auth from '@react-native-firebase/auth'
 
-firestore().settings({ host: 'http://localhost:8080', ssl: false, persistence: false, cacheSizeBytes: 100000000 })
+// firestore().settings({ host: 'http://localhost:8080', ssl: false, persistence: false, cacheSizeBytes: 100000000 })
 
 firestore.setLogLevel("debug")
 
